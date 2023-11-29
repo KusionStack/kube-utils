1c21aef4e8607a859e575fe0414dcfd91240041b
zoumo <jim.zoumo@gmail.com>
2023-11-29 18:29:39 +0800

    feat: support override and hide initializer

--- controller/initializer/initializer_test.go
+++ controller/initializer/initializer_test.go
@@ -24,24 +24,30 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-func Test_Initialzier(t *testing.T) {
+func Test_Initializer(t *testing.T) {
 	initializer := New()
 	err := initializer.Add("test1", testInitFunc)
 	assert.NoError(t, err)
 	err = initializer.Add("test2", testInitFunc)
 	assert.NoError(t, err)
-	err = initializer.Add("test3", testInitFunc, WithDisableByDefault())
+	err = initializer.Add("test3", testInitFunc)
+	assert.NoError(t, err)
+	err = initializer.Add("__internal", testInitFunc, WithHidden(), WithDisableByDefault())
 	assert.NoError(t, err)
 
-	controllers := initializer.KnownControllers()
-	assert.EqualValues(t, []string{"test1", "test2", "test3"}, controllers)
+	// disable override
+	err = initializer.Add("test3", testInitFunc)
+	assert.Error(t, err)
+	// allow override
+	err = initializer.Add("test3", testInitFunc, WithOverride(), WithDisableByDefault())
+	assert.NoError(t, err)
+
+	names := initializer.Knowns()
+	assert.EqualValues(t, []string{"__internal", "test1", "test2", "test3"}, names)
 	assert.True(t, initializer.Enabled("test1"))
 	assert.True(t, initializer.Enabled("test2"))
 	assert.False(t, initializer.Enabled("test3"))
-
-	// duplicate
-	err = initializer.Add("test1", testInitFunc)
-	assert.Error(t, err)
+	assert.True(t, initializer.Enabled("__internal"))
 
 	// test bind flag
 	fs := pflag.NewFlagSet("test-*", pflag.PanicOnError)
@@ -52,6 +58,7 @@ func Test_Initialzier(t *testing.T) {
 	assert.True(t, initializer.Enabled("test1"))
 	assert.True(t, initializer.Enabled("test2"))
 	assert.False(t, initializer.Enabled("test3"))
+	assert.True(t, initializer.Enabled("__internal"))
 
 	fs = pflag.NewFlagSet("test", pflag.PanicOnError)
 	initializer.BindFlag(fs)
@@ -61,6 +68,7 @@ func Test_Initialzier(t *testing.T) {
 	assert.True(t, initializer.Enabled("test1"))
 	assert.True(t, initializer.Enabled("test2"))
 	assert.False(t, initializer.Enabled("test3"))
+	assert.True(t, initializer.Enabled("__internal"))
 
 	fs = pflag.NewFlagSet("test", pflag.PanicOnError)
 	initializer.BindFlag(fs)
@@ -70,15 +78,17 @@ func Test_Initialzier(t *testing.T) {
 	assert.False(t, initializer.Enabled("test1"))
 	assert.False(t, initializer.Enabled("test2"))
 	assert.True(t, initializer.Enabled("test3"))
+	assert.True(t, initializer.Enabled("__internal"))
 
 	fs = pflag.NewFlagSet("test", pflag.PanicOnError)
 	initializer.BindFlag(fs)
-	fs.Set("controllers", "-test1")
+	fs.Set("controllers", "-test1,-__internal")
 	err = fs.Parse(nil)
 	assert.NoError(t, err)
 	assert.False(t, initializer.Enabled("test1"))
 	assert.False(t, initializer.Enabled("test2"))
 	assert.False(t, initializer.Enabled("test3"))
+	assert.True(t, initializer.Enabled("__internal"))
 }
 
 func testInitFunc(manager.Manager) (enabled bool, err error) {
