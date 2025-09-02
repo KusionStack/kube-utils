b01c19c8ba168b281313408ad090f292fb136904
ColdsteelRail <574252631@qq.com>
2025-09-02 13:30:42 +0800

    add inactive targets

--- xset/xcontrol/target_control.go
+++ xset/xcontrol/target_control.go
@@ -97,7 +97,7 @@ func (r *targetControl) GetFilteredTargets(ctx context.Context, selector *metav1
 		return nil, fmt.Errorf("target list items is invalid")
 	}
 
-	// todo filterOutInactiveTargets
+	items = filterOutInactiveTargets(r.xsetController, items)
 	targets, err := r.getTargets(items, selector, owner)
 	return targets, err
 }
@@ -198,4 +198,15 @@ func setUpCache(cache cache.Cache, controller api.XSetController) error {
 		return fmt.Errorf("failed to index by field %s: %s", FieldIndexOwnerRefUID, err.Error())
 	}
 	return nil
+}
+
+func filterOutInactiveTargets(xsetController api.XSetController, targets []client.Object) []client.Object {
+	var filteredTarget []client.Object
+	for i := range targets {
+		target := targets[i]
+		if xsetController.CheckInactive(target) {
+			filteredTarget = append(filteredTarget, target)
+		}
+	}
+	return filteredTarget
 }
